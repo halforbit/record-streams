26b693e2f59604062024ef6e6f0f3e8bb5977fd5
Jim Noble <jim@halforbit.com>
2019-11-21 03:19:26 -0500

    impl RecordSerializer

---
+++ Halforbit.RecordStreams/Serialization/RecordSerializer.cs
@@ -0,0 +1,33 @@
+﻿using Halforbit.BitBuffers;
+using Halforbit.RecordStreams.Serialization.Interface;
+using System.Buffers;
+using System.Threading.Tasks;
+
+namespace Halforbit.RecordStreams.Serialization
+{
+    public abstract class RecordSerializer<TRecordValue> : IRecordSerializer
+    {
+        public Task<(TRecord record, long bytesRead)> Deserialize<TRecord>(ReadOnlySequence<byte> bytes)
+        {
+            try
+            {
+                var (record, bytesRead) = Deserialize(bytes.ToArray());
+
+                return Task.FromResult(((TRecord)(object)record, bytesRead));
+            }
+            catch (ReadOverflowException)
+            {
+                return Task.FromResult((default(TRecord), 0L));
+            }
+        }
+
+        public Task<ReadOnlySequence<byte>> Serialize<TRecord>(TRecord record)
+        {
+            return Task.FromResult(new ReadOnlySequence<byte>(Serialize((TRecordValue)(object)record)));
+        }
+
+        protected abstract (TRecordValue Record, long BytesRead) Deserialize(byte[] data);
+
+        protected abstract byte[] Serialize(TRecordValue value);
+    }
+}
